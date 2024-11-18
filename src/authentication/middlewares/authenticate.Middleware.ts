e09754f81c7c80b277f9d6f733224f0e2b76010e
thiago9510 <thiagobatistalucas@gmail.com>
2024-11-17 21:53:57 -0300

    autenticação implementada

---
+++ src/authentication/middlewares/authenticate.Middleware.ts
@@ -0,0 +1,46 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
+export const authenticateMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    try { 
+        const secretJWT: string | undefined = process.env.SECRET_JWT_KEY
+        if(!secretJWT){
+            return res.status(400).json({
+                sucess: false,
+                menssagem: 'Erro Interno do Servidor: Variável SECRET_JWT_KEY Não definida'
+            })
+        }
+        const authValue = req.headers['authorization'] as string
+        if(!authValue){
+            return res.status(401).json({
+                sucess: false,
+                menssagem: 'Token Inválido'
+            })
+        }
+
+        const hashToken = authValue.split(' ')
+        if(hashToken.length !== 2 || hashToken[0] !== 'Bearer'){
+            return res.status(401).json({
+                sucess: false,
+                menssagem: 'Token Inválido'
+            })
+        }
+
+        //decodificar token se tudo certo
+        const token = hashToken[1]
+        const decoded = jwt.verify(token, secretJWT) as JwtPayload
+        req.user = {usuario_id: decoded} as any //alterar quando defifir o formato do token
+        
+        
+        next()       
+    } catch (error) {
+        return res.status(403).json({
+            sucess: false,
+            menssagem: error
+        })
+    }
+}
